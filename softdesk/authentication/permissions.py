66bf59ee1c0bb5ee603e7707a266527098f9a4be
8astien <bastien.cauquil@pm.me>
2024-08-24 12:56:37 +0200

    Updated README

--- softdesk/authentication/permissions.py
+++ softdesk/authentication/permissions.py
@@ -2,19 +2,10 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class IsOwner(BasePermission):
-    """
-    Object-level permission to only allow owners of an object to edit it.
-    Assumes the model instance has an `author` attribute.
-    IsOwnerOrReadOnly dans la doc DRF
-    """
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return True
         return request.user and request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-
-        # Write permissions are only allowed to the owner of the object.
         return obj == request.user or request.user.is_superuser
